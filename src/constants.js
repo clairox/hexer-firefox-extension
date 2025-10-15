40e2e4ab5ec12630db7a295f419105845b2c71ea
clairox <33704909+clairox@users.noreply.github.com>
2025-10-15 09:51:32 -0400

    refactor: move source code into one file

--- src/constants.js
+++
@@ -1,2 +0,0 @@
-const HEX_MATCHER = /#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}/;
-const TOOLTIP_OFFSET = 70;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import { displayTooltip, initializeTooltip, showTooltip } from "./tooltip";
+const HEX_MATCHER = /#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}/;
+const TOOLTIP_OFFSET = 70;
+const TOOLTIP_DELAY = 500;
 
 const tooltip = initializeTooltip();
+const showTooltip = false;
+let timeoutID = null;
+
 document.body.appendChild(tooltip);
 
-document.addEventListener("mousemove", (event) => {
+document.addEventListener("mousemove", function (event) {
+    if (showTooltip && timeoutID) {
+        clearTimeout(timeoutID);
+        showTooltip = false;
+        return;
+    }
+    timeoutID = setTimeout(() => handleMouseMove(event), TOOLTIP_DELAY);
+});
+
+function handleMouseMove(event) {
     const caretPosition = document.caretPositionFromPoint(
         event.clientX,
         event.clientY,
@@ -26,13 +40,41 @@ document.addEventListener("mousemove", (event) => {
             displayTooltip(result[0], event.clientX, event.clientY);
         }
     }
-});
+}
 
-const getWordAt = (text, position) => {
+function getWordAt(text, position) {
     const left = text.slice(0, position).search(/\S+$/);
     const right = text.slice(position).search(/\s/);
     if (right < 0) {
         return text.slice(left);
     }
     return text.slice(left, position + right);
-};
+}
+
+function displayTooltip(hex, x, y) {
+    tooltip.style.backgroundColor = hex;
+    tooltip.style.left = x - tooltip.offsetWidth / 2 + TOOLTIP_OFFSET + "px";
+    tooltip.style.top = y - tooltip.offsetHeight / 2 - TOOLTIP_OFFSET + "px";
+    tooltip.style.display = "block";
+    showTooltip = true;
+}
+
+function hideTooltip() {
+    tooltip.style.backgroundColor = "unset";
+    tooltip.style.left = "unset";
+    tooltip.style.top = "unset";
+    tooltip.style.display = "none";
+    showTooltip = false;
+}
+
+function initializeTooltip() {
+    const tooltip = document.createElement("div");
+    tooltip.id = "hexer_tooltip";
+    tooltip.style.width = "100px";
+    tooltip.style.height = "100px";
+    tooltip.style.position = "fixed";
+    tooltip.style.zIndex = "10000";
+    tooltip.style.display = "hidden";
+
+    return tooltip;
+}
